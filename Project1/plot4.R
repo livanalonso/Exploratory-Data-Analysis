28e3abb63d19af71f1190af195b9fa46c9c9387b
Livan Alonso <livan.alonso@gmail.com>
2014-10-10 18:49:18 -0400

    Update plot4.R

--- Project1/plot4.R
+++ Project1/plot4.R
@@ -1,4 +1,4 @@
-# Download the data of project
+# Download the data
 fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl,destfile="dataset.zip",method="curl")
         
@@ -14,11 +14,11 @@ data<-read.table("household_power_consumption.txt",header = TRUE,sep=";",na.stri
 ## Combine Date and Time column and create new column
 data$Date.Time<-strptime(paste(data$Date,data$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 
+# Subset data 
 data$Date<-as.Date(as.Date(data$Date,"%d/%m/%Y"))
 subset.data<-subset(data,as.POSIXct(Date)==as.POSIXct(as.Date("01/02/2007","%d/%m/%Y")) | as.POSIXct(Date)==as.POSIXct(as.Date("02/02/2007","%d/%m/%Y")))
 
 png(file="plot4.png",width = 480, height = 480)
-
 par(mfcol = c(2, 2))
 with(subset.data,{
         plot(Date.Time,Global_active_power,xlab="\n",ylab="Global Active Power",type="l")
@@ -29,6 +29,7 @@ with(subset.data,{
         legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
         plot(Date.Time,Voltage,xlab="datetime",ylab="Voltage",type="l")
+        
         plot(Date.Time,Global_reactive_power,xlab="datetime",ylab="Global_reactive_power",type="l")
         
         }
