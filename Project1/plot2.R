a9973943e52fa1aa724171feb4dce1993edd9eeb
Livan Alonso <livan.alonso@gmail.com>
2014-10-11 21:38:33 -0400

    Update plot2.R

--- Project1/plot2.R
+++ Project1/plot2.R
@@ -18,6 +18,7 @@ data$Date.Time<-strptime(paste(data$Date,data$Time, sep=" "),"%d/%m/%Y %H:%M:%S"
 data$Date<-as.Date(as.Date(data$Date,"%d/%m/%Y"))
 subset.data<-subset(data,as.POSIXct(Date)==as.POSIXct(as.Date("01/02/2007","%d/%m/%Y")) | as.POSIXct(Date)==as.POSIXct(as.Date("02/02/2007","%d/%m/%Y")))
 
+# Plot data
 png(file="plot2.png",width = 480, height = 480)
 with(subset.data,plot(Date.Time,Global_active_power,xlab="\n",ylab="Global Active Power (kilowatts)",type="l"))
 dev.off()
